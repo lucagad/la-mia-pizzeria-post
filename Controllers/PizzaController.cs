887ce7515506f659edc407a97dfd634b376702ad
lucagaddini <lucagaddini@icloud.com>
2022-10-05 15:53:04 +0200

    Modificata View Index con dati prelevati dal DB

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -7,16 +7,25 @@ public class PizzaController : Controller
 {
     public IActionResult Index()
     {
-        List<Pizza> listaPizze = new List<Pizza>();
+        List<Pizza> pizzes;
+        
+        using (PizzaContext db = new PizzaContext())
+        {
+            pizzes = db.Pizzas.ToList<Pizza>();
+        }
 
-        return View("Index", listaPizze);
+        return View("Index", pizzes);
     }
     
     public IActionResult Show(int id)
     {
-        List<Pizza> listaPizze = new List<Pizza>();
-
-        return View("Show",listaPizze[id]);
+        Pizza pizzaSelected;
+        
+        using (PizzaContext db = new PizzaContext())
+        {
+            pizzaSelected = db.Pizzas.Where(pizza => pizza.PizzaId == id).First();
+        }
+        return View("Show",pizzaSelected);
     }
     
     public IActionResult Create()
